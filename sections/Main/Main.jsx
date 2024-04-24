0084b8c75b02880b1b5eb722aad6ae5c1249cb39
borislavpachev <borislav.pachev@gmail.com>
2024-04-24 14:59:30 +0300

    card props for the main selection added with sample text

--- sections/Main/Main.jsx
+++ sections/Main/Main.jsx
@@ -3,7 +3,39 @@ import {
   StyledDescription,
   StyledTextContainer,
   StyledTitle,
+  StyledInnerContainer,
+  StyledImageContainer,
+  StyledCardContainer,
+  StyledParagraph,
 } from '../../sections/Main/elements';
+import { Card } from '../../collections/Card/Card';
+
+import Image from 'next/image';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faShapes,
+  faMagnifyingGlass,
+  faPeopleGroup,
+} from '@fortawesome/free-solid-svg-icons';
+
+const cardProps = [
+  {
+    logo: <FontAwesomeIcon icon={faShapes} />,
+    title: 'Brief',
+    text: 1,
+  },
+  {
+    logo: <FontAwesomeIcon icon={faMagnifyingGlass} />,
+    title: 'Search',
+    text: 1,
+  },
+  {
+    logo: <FontAwesomeIcon icon={faPeopleGroup} />,
+    title: 'Pitch',
+    text: 1,
+  },
+];
 
 export const Main = ({ image, title, description }) => {
   return (
@@ -12,6 +44,22 @@ export const Main = ({ image, title, description }) => {
         <StyledTitle>{title}</StyledTitle>
         <StyledDescription>{description}</StyledDescription>
       </StyledTextContainer>
+      <StyledInnerContainer>
+        <StyledImageContainer>
+          <Image
+            layout="responsive"
+            src={image.src}
+            alt={image.alt}
+            width={image.width}
+            height={image.height}
+          />
+        </StyledImageContainer>
+        <StyledCardContainer>
+          {cardProps.map((card) => (
+            <Card key={card.title} {...card} />
+          ))}
+        </StyledCardContainer>
+      </StyledInnerContainer>
     </StyledContainer>
   );
 };
