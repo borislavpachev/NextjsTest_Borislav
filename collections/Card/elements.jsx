4c72fbb217aa879a6983e31adbbf37e0f9cc951b
borislavpachev <borislav.pachev@gmail.com>
2024-04-24 14:53:44 +0300

    card container element added

--- collections/Card/elements.jsx
+++ collections/Card/elements.jsx
@@ -1,1 +1,13 @@
-// Styled elements for the Card go here
+import styled from 'styled-components';
+import {
+    SectionContainer,
+  } from '~/components';
+
+export const StyledContainer = styled(
+  ({ height, variant = 'contained', color = 'main', ...props }) => (
+    <SectionContainer {...props} />
+  )
+)`
+  align-items: center;
+  background-color: #f9f9f9;
+`;
